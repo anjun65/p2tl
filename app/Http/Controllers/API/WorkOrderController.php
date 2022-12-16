b443d5bcba4b55902bfd8e7d7bbd6904538e400e
anjun65 <hajrulkhaira@gmail.com>
2022-12-16 16:18:06 +0700

    rev

--- app/Http/Controllers/API/WorkOrderController.php
+++ app/Http/Controllers/API/WorkOrderController.php
@@ -29,32 +29,14 @@ class WorkOrderController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'users_id' => ['required', 'string', 'max:255'],
-            'id_pelanggan' => ['required', 'string', 'max:255'],
-            'nama_pelanggan' => ['required', 'string', 'max:255'],
-            'latitude' => ['required', 'number', 'max:255'],
-            'longitude' => ['required', 'number', 'max:255'],
-            'alamat' => ['required', 'number', 'max:255'],
-            'tarif' => ['required', 'number', 'max:255'],
-            'daya' => ['required', 'number', 'max:255'],
-            'rbm' => ['required', 'number', 'max:255'],
-            'lgkh' => ['required', 'number', 'max:255'],
-            'fkm' => ['required', 'number', 'max:255'],
+            'id' => ['required'],
             'keterangan_p2tl' => ['required', 'string', 'max:255'],
         ]);
 
-        $workorder = WorkOrder::create([
-            'users_id' => $request->users_id,
-            'id_pelanggan' => $request->id_pelanggan,
-            'nama_pelanggan' => $request->nama_pelanggan,
-            'latitude' => $request->latitude,
-            'longitude' => $request->longitude,
-            'alamat' => $request->alamat,
-            'tarif' => $request->tarif,
-            'daya' => $request->daya,
-            'rbm' => $request->rbm,
-            'lgkh' => $request->lgkh,
-            'fkm' => $request->fkm,
+        
+        $workorder = WorkOrder::find($request->id);
+        
+        $workorder->update([
             'keterangan_p2tl' => $request->keterangan_p2tl,
         ]);
 
